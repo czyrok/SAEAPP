d06d6f89468461beeaa5a5cfbc171dcc49e1e93d
eduardo monteiro <edoumnt47@outlook.fr>
2022-01-14 16:46:58 +0100

    Init main

--- SAEAPP/SAEAPP.cpp
+++ SAEAPP/SAEAPP.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+
+const int NBstats = 100;
+struct bambous {
+
+	float indDPousse;
+	float vitesse_pousse;
+};
+const bambous NBbambous[100];
+
+struct panda {
+	int x;
+};
+
+
+struct jardin {
+	bambous bambou;
+	panda  pandas;
+	int NBbambous;
+	float stat[3][NBstats];
+	char nomAlgo[0];
+};
+
+const int TailleMaxBambous;
+const int NBDeplacementLimite = -1;
