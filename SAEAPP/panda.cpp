316d53f1f27aa1b72ad8fa761879a486bc8cee3c
Dylan <63360083+czyrok@users.noreply.github.com>
2022-01-16 22:16:38 +0100

    Update panda.cpp
    Amélioration de la fonction deplacementPanda

--- SAEAPP/panda.cpp
+++ SAEAPP/panda.cpp
@@ -1,19 +1,56 @@
 #pragma once
-#include <cstring>
 using namespace std;
 
 #include "var.h"
 
-int deplacementPanda(jardins& jardin, int indPanda, int x) {
-	jardin.pandas[indPanda].x = x;
+bool deplacementPanda(jardins &jardin, int indPanda, int x)
+{
+	if (jardin.pandas[indPanda].x != x)
+	{
+		if (jardin.pandaDeplacementLimite == -1)
+		{
+			jardin.pandas[indPanda].x = x;
+		}
+		else
+		{
+			if (jardin.pandas[indPanda].x < x)
+			{
+				int NBDeplacementLimite = jardin.pandaDeplacementLimite;
 
-	return 1;
+				while (NBDeplacementLimite > 0 && jardin.pandas[indPanda].x != x)
+				{
+					jardin.pandas[indPanda].x++;
+					NBDeplacementLimite--;
+				}
+			}
+			else
+			{
+				int NBDeplacementLimite = jardin.pandaDeplacementLimite;
+
+				while (NBDeplacementLimite > 0 && jardin.pandas[indPanda].x != x)
+				{
+					jardin.pandas[indPanda].x--;
+					NBDeplacementLimite--;
+				}
+			}
+		}
+	}
+
+	if (jardin.pandas[indPanda].x != x)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 }
 
-void actualiserAffichagePanda(jardins& jardin) {
-
+void actualiserAffichagePanda(jardins &jardin)
+{
 }
 
-void pandaCoupeBambou(jardins& jardin, int indPanda) {
+void pandaCoupeBambou(jardins &jardin, int indPanda)
+{
 	jardin.bambous[jardin.pandas[indPanda].x].taillePousse = 0;
 }
