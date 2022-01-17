a6168de1b1009cc3368e10fcc8187563f0f7035e
Dylan Czyrok <czyrok@outlook.fr>
2022-01-17 17:19:10 +0100

    Ajout de l'algo ReduceFastest

--- SAEAPP/algo.cpp
+++ SAEAPP/algo.cpp
@@ -14,14 +14,39 @@ void couperBambou(jardins& jardin) {
 			}
 		}
 
-		int statut = deplacementPanda(jardin, 0, indMax);
+		bool statut = deplacementPanda(jardin, 0, indMax);
 
 		if (statut == true) {
 			pandaCoupeBambou(jardin, 0);
 		}
 	}
 	else if (strcmp(jardin.nomAlgo, "ReduceFastest") == 0) {
+		int sommeVitessePousse = 0;
 
+		for (int i = 0; i < jardin.NBBambous; i++) {
+			sommeVitessePousse += jardin.bambous[i].vitessePousse;
+		}
+
+		int indBambou = -1;
+
+		for (int i = 0; i < jardin.NBBambous; i++) {
+			if (jardin.bambous[i].taillePousse * jardin.bambous[i].vitessePousse > sommeVitessePousse * 1.45 && indBambou == -1) {
+				indBambou = i;
+			}
+			else if(jardin.bambous[i].taillePousse * jardin.bambous[i].vitessePousse > sommeVitessePousse){
+				if (jardin.bambous[i].taillePousse * jardin.bambous[i].vitessePousse > jardin.bambous[indBambou].taillePousse * jardin.bambous[indBambou].vitessePousse) {
+					indBambou = i;
+				}
+			}
+		}
+
+		if (indBambou != -1) {
+			bool statut = deplacementPanda(jardin, 0, indBambou);
+
+			if (statut == true) {
+				pandaCoupeBambou(jardin, 0);
+			}
+		}
 	}
 
 	//actualiserStats(jardin);
