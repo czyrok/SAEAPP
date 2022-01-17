2219669f3282ffb406f2404765d5728d9ca8b5d9
maxime barber <maxime.b03@outlook.fr>
2022-01-17 14:50:14 +0100

    fonction actualiserAffichageStatistiques

--- SAEAPP/visuel.cpp
+++ SAEAPP/visuel.cpp
@@ -68,3 +68,36 @@ void actualiserAffichageBambous(jardins& jardin, SDL_Renderer* rendu)
 
 	SDL_RenderPresent(rendu);
 }
+
+void actualiserAffichageStatistiques(jardins& jardin, SDL_Renderer* rendu)
+{
+	int x = jardin.indStat;
+	float taillemax = jardin.tailleMaxStat[jardin.indStat];
+	float taillemin = jardin.tailleMinStat[jardin.indStat];
+	float taillemoy = jardin.tailleMoyStat[jardin.indStat];
+	actualiserStats(jardin);
+	SDL_SetRenderDrawColor(rendu, 255, 0, 0, 255); //max rouge
+	SDL_RenderDrawLine(rendu, x, taillemax, jardin.indStat, jardin.tailleMaxStat[jardin.indStat]);
+	SDL_Rect pointmax;
+	pointmax.x = 0 + x;
+	pointmax.y = TAILLEFENY - taillemax;
+	pointmax.w = 0 + jardin.indStat;
+	pointmax.h = TAILLEFENY - jardin.tailleMaxStat[jardin.indStat];
+	SDL_RenderFillRect(rendu, &pointmax);
+	SDL_SetRenderDrawColor(rendu, 0, 255, 0, 255); //min vert
+	SDL_Rect pointmin;
+	pointmin.x = 0 + x;
+	pointmin.y = TAILLEFENY - taillemin;
+	pointmin.w = 0 + jardin.indStat;
+	pointmin.h = TAILLEFENY - jardin.tailleMinStat[jardin.indStat];
+	SDL_RenderDrawLine(rendu, x, taillemin, jardin.indStat, jardin.tailleMinStat[jardin.indStat]);
+	SDL_RenderFillRect(rendu, &pointmin);
+	SDL_SetRenderDrawColor(rendu, 0, 0, 255, 255); //moyenne bleu
+	SDL_RenderDrawLine(rendu, x, taillemoy, jardin.indStat, jardin.tailleMoyStat[jardin.indStat]);
+	SDL_Rect pointmoyenne;
+	pointmoyenne.x = 0 + x;
+	pointmoyenne.y = TAILLEFENY - taillemoy;
+	pointmoyenne.w = 0 + jardin.indStat;
+	pointmoyenne.h = TAILLEFENY - jardin.tailleMoyStat[jardin.indStat];
+	SDL_RenderFillRect(rendu, &pointmoyenne);
+}
