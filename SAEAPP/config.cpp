c7f7124851f1adf629e2cc9819d539b6433d34cf
eduardo monteiro <edoumnt47@outlook.fr>
2022-01-17 15:43:50 +0100

    Init config.cpp

--- SAEAPP/config.cpp
+++ SAEAPP/config.cpp
@@ -28,16 +28,18 @@ void importer(jardins jardins[], int& NBJardins) {
 
 
             if (selection == 'b') {
-                float vitessePousse = 0;
-
                 char ligneBambou[1000];
                 int indCarac = 0;
 
                 for (int i = 2; i < ligneTaille; i++)
                 {
+                    
                     ligneBambou[indCarac] = ligne[i];
                     indCarac++;
                 }
+
+                float vitessePousse = atof(ligneBambou);
+                initBambous(bambous, NBBambous, vitessePousse);
             }
             else if (selection == 'p') {
                 initPandas(pandas, NBPandas, 0);
@@ -45,7 +47,43 @@ void importer(jardins jardins[], int& NBJardins) {
             else if (selection == 'j') {
                 strcpy_s(ligneJardin, ligne);
             }
+        
         }
+
+
+        int tailleLigneJardin = strlen(ligneJardin);
+        char params[4][50];
+        int indCarac = 0;
+        int NBVirgules = 0;
+
+        for (int i = 2; i < tailleLigneJardin, i++) {
+            params[NBVirgules][indCarac] = ligneJardin[i];
+        }
+
         entree.close();
     }
+}
+
+//while (i <NBJardins ) {
+//    char ligne[10];
+//    entree.getline(ligne, 10, ',');
+//    jardins[i].dpt = atoi(ligne);
+//    entree.getline(jardins[i]., 100, ',');
+//    entree.getline(jardins[i].indStat, 100, ',');
+//    entree.getline(jardins[i].nomAlgo, 100,',');
+//    entree.getline(jardins[i].manuelActive, 100,',');
+//    i++;
+//}
+//  
+
+
+
+
+
+
+if (NBVirgules == ',') {
+    NBVirgules++;
+else {
+    indCarac++;
+}
 }
