78625e331ee0710173329dedc5c9b22cdca915ad
Dylan Czyrok <czyrok@outlook.fr>
2022-01-17 17:19:32 +0100

    Finalisation de la fonction importerConfig

--- SAEAPP/config.cpp
+++ SAEAPP/config.cpp
@@ -1,16 +1,16 @@
-#include<fstream>
-#include<iostream>
-#include"var.h"
-using namespace std; 
+#pragma once
+using namespace std;
 
-void importer(jardins jardins[], int& NBJardins) {
-    ifstream entree("config.txt", ios::in);
+#include<fstream>
+
+#include "var.h"
+
+void importerConfig(jardins jardins[], int& NBJardins) {
+    ifstream config("config.txt", ios::in);
 
     char ligne[1000];
 
-    if (!entree)
-        cout << "Probleme d'ouverture \n";
-    else {
+    if (config) {
         char ligneJardin[1000];
 
         bambous bambous[maxNBBambous];
@@ -19,13 +19,12 @@ void importer(jardins jardins[], int& NBJardins) {
         pandas pandas[maxNBPandas];
         int NBPandas = 0;
 
-        while (!entree.eof()) {
-            entree.getline(ligne, 1000);
+        while (!config.eof()) {
+            config.getline(ligne, 1000);
 
             int ligneTaille = strlen(ligne);
 
             char selection = ligne[0];
-
 
             if (selection == 'b') {
                 char ligneBambou[1000];
@@ -33,7 +32,6 @@ void importer(jardins jardins[], int& NBJardins) {
 
                 for (int i = 2; i < ligneTaille; i++)
                 {
-                    
                     ligneBambou[indCarac] = ligne[i];
                     indCarac++;
                 }
@@ -47,43 +45,29 @@ void importer(jardins jardins[], int& NBJardins) {
             else if (selection == 'j') {
                 strcpy_s(ligneJardin, ligne);
             }
-        
         }
-
 
         int tailleLigneJardin = strlen(ligneJardin);
-        char params[4][50];
-        int indCarac = 0;
-        int NBVirgules = 0;
+        char paramsJardin[4][50];
+        int indCaracLigneJardin = 0;
+        int NBVirgulesLigneJardin = 0;
 
-        for (int i = 2; i < tailleLigneJardin, i++) {
-            params[NBVirgules][indCarac] = ligneJardin[i];
+        for (int i = 2; i < tailleLigneJardin; i++) {
+            if (ligneJardin[i] == ',') {
+                paramsJardin[NBVirgulesLigneJardin][indCaracLigneJardin] = '\0';
+
+                NBVirgulesLigneJardin++;
+                indCaracLigneJardin = 0;
+            }
+            else {
+                paramsJardin[NBVirgulesLigneJardin][indCaracLigneJardin] = ligneJardin[i];
+
+                indCaracLigneJardin++;
+            }
         }
 
-        entree.close();
+        initJardin(jardins, NBJardins, paramsJardin[0], bambous, NBBambous, pandas, NBPandas, atoi(paramsJardin[1]), paramsJardin[2], atoi(paramsJardin[3]));
+
+        config.close();
     }
-}
-
-//while (i <NBJardins ) {
-//    char ligne[10];
-//    entree.getline(ligne, 10, ',');
-//    jardins[i].dpt = atoi(ligne);
-//    entree.getline(jardins[i]., 100, ',');
-//    entree.getline(jardins[i].indStat, 100, ',');
-//    entree.getline(jardins[i].nomAlgo, 100,',');
-//    entree.getline(jardins[i].manuelActive, 100,',');
-//    i++;
-//}
-//  
-
-
-
-
-
-
-if (NBVirgules == ',') {
-    NBVirgules++;
-else {
-    indCarac++;
-}
 }
