b9d862259068f865983e3deea83b59957c98ae56
Dylan Czyrok <czyrok@outlook.fr>
2022-01-18 09:58:55 +0100

    Ajout de constantes et de prototypes

--- SAEAPP/var.h
+++ SAEAPP/var.h
@@ -48,6 +48,8 @@ struct jardins {
 	char nomAlgo[30];
 
 	bool manuelActive;
+
+	float paramPourReduceFastest = 1.45;
 };
 
 const int TAILLEBARY = 50;
@@ -107,5 +109,33 @@ void afficherBoutonsBar(SDL_Renderer*, TTF_Font*, SDL_Rect[], char[][NBMaxCaracB
 void afficherTexteBoutonBar(SDL_Renderer*, TTF_Font*, SDL_Rect[], char[][NBMaxCaracBoutons], int, int);
 void afficherIMGBoutonBar(SDL_Renderer*, TTF_Font*, SDL_Rect[], char[][NBMaxCaracBoutons], int, int);
 
+void afficherBoutonsMenu(SDL_Renderer*, TTF_Font*, SDL_Rect[], char[][NBMaxCaracBoutons], int);
+void afficherBoutonsMenu(SDL_Renderer*, TTF_Font*, SDL_Rect[], char[][NBMaxCaracBoutons], int, int);
 
 void importerConfig(jardins[], int&);
+
+SDL_Texture* loadText(SDL_Renderer* rendu, const char* text, SDL_Color coul, TTF_Font* font) {
+	SDL_Surface* surface = TTF_RenderText_Solid(font, text, coul);
+
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(rendu, surface);
+	SDL_FreeSurface(surface);
+	return texture;
+}
+
+SDL_Texture* loadImage(SDL_Renderer* rendu, const char* name)
+{
+	/* Charge les images avec SDL Image dans une SDL_Surface */
+	SDL_Surface* loadedImage = NULL;
+	SDL_Texture* texture = NULL;
+	loadedImage = IMG_Load(name);
+	SDL_SetSurfaceAlphaMod(loadedImage, 200);//transparence à 200
+	if (loadedImage != NULL)
+	{
+		// Conversion de l'image en texture
+		texture = SDL_CreateTextureFromSurface(rendu, loadedImage);
+		// On se débarrasse du pointeur vers une surface
+		SDL_FreeSurface(loadedImage);
+		loadedImage = NULL;
+	}
+	return texture;
+}
