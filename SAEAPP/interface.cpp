081d5fe8cbedde5f7c4c98c017666c5a68cb5877
Dylan Czyrok <czyrok@outlook.fr>
2022-01-18 10:04:59 +0100

    Ajout de fonctions et changement de nom de fichier

--- SAEAPP/interface.cpp
+++ SAEAPP/interface.cpp
@@ -4,35 +4,10 @@ using namespace std;
 #include <cstring>
 
 #include "var.h"
+#include "exterieur.cpp"
 
-SDL_Texture* loadText(SDL_Renderer* rendu, const char* text, SDL_Color coul, TTF_Font* font) {
-	SDL_Surface* surface = TTF_RenderText_Solid(font, text, coul);
-
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(rendu, surface);
-	SDL_FreeSurface(surface);
-	return texture;
-}
-
-SDL_Texture* loadImage(SDL_Renderer* rendu, const char* name)
-{
-	/* Charge les images avec SDL Image dans une SDL_Surface */
-	SDL_Surface* loadedImage = NULL;
-	SDL_Texture* texture = NULL;
-	loadedImage = IMG_Load(name);
-	SDL_SetSurfaceAlphaMod(loadedImage, 200);//transparence à 200
-	if (loadedImage != NULL)
-	{
-		// Conversion de l'image en texture
-		texture = SDL_CreateTextureFromSurface(rendu, loadedImage);
-		// On se débarrasse du pointeur vers une surface
-		SDL_FreeSurface(loadedImage);
-		loadedImage = NULL;
-	}
-	return texture;
-}
-
-void afficherBoutonsBar(SDL_Renderer* rendu, TTF_Font* police, SDL_Rect rectBoutonsBar[], char boutonsBar[][NBMaxCaracBoutons], int NBButonsBar) {
-	for (int i = 0; i < NBButonsBar; i++) {
+void afficherBoutonsBar(SDL_Renderer* rendu, TTF_Font* police, SDL_Rect rectBoutonsBar[], char boutonsBar[][NBMaxCaracBoutons], int NBBoutonsBar) {
+	for (int i = 0; i < NBBoutonsBar; i++) {
 		if (i == 0) {
 			afficherIMGBoutonBar(rendu, police, rectBoutonsBar, boutonsBar, i, 0);
 		}
@@ -45,7 +20,7 @@ void afficherBoutonsBar(SDL_Renderer* rendu, TTF_Font* police, SDL_Rect rectBout
 }
 
 void afficherTexteBoutonBar(SDL_Renderer* rendu, TTF_Font* police, SDL_Rect rectBoutonsBar[], char boutonsBar[][NBMaxCaracBoutons], int indBouton, int departX) {
-	SDL_Texture* texture = loadText(rendu, boutonsBar[indBouton], { 0, 0, 0 }, police);
+	SDL_Texture* texture = loadText(rendu, boutonsBar[indBouton], { 255, 255, 255 }, police);
 
 	int positionTexteW, positionTexteH;
 	SDL_QueryTexture(texture, NULL, NULL, &positionTexteW, &positionTexteH);
@@ -72,4 +47,31 @@ void afficherIMGBoutonBar(SDL_Renderer* rendu, TTF_Font* police, SDL_Rect rectBo
 	rectBoutonsBar[indBouton] = positionIMG;
 
 	afficherTexteBoutonBar(rendu, police, rectBoutonsBar, boutonsBar, indBouton, departX + 30);
+}
+
+void afficherBoutonsMenu(SDL_Renderer* rendu, TTF_Font* police, SDL_Rect rectBoutonsMenu[], char boutonsMenu[][NBMaxCaracBoutons], int NBBoutonsMenu) {
+	int positionY = 0;
+
+	for (int i = 0; i < NBBoutonsMenu; i++) {
+		if (i == 0) {
+			afficherBoutonsMenu(rendu, police, rectBoutonsMenu, boutonsMenu, i, 0);
+		}
+		else {
+			afficherBoutonsMenu(rendu, police, rectBoutonsMenu, boutonsMenu, i, rectBoutonsMenu[i - 1].y + rectBoutonsMenu[i - 1].h);
+		}
+	}
+
+	SDL_RenderPresent(rendu);
+}
+
+void afficherBoutonsMenu(SDL_Renderer* rendu, TTF_Font* police, SDL_Rect rectBoutonsMenu[], char boutonsMenu[][NBMaxCaracBoutons], int indBouton, int departY) {
+	SDL_Texture* texture = loadText(rendu, boutonsMenu[indBouton], { 255, 255, 255 }, police);
+
+	int positionTexteW, positionTexteH;
+	SDL_QueryTexture(texture, NULL, NULL, &positionTexteW, &positionTexteH);
+
+	SDL_Rect positionTexte = { TAILLEMENUX / 2, TAILLEMENUX / 2 + departY, positionTexteW, positionTexteH };
+	SDL_RenderCopy(rendu, texture, NULL, &positionTexte);
+
+	rectBoutonsMenu[indBouton] = positionTexte;
 }
